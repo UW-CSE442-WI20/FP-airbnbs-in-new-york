c5c1c10506af3a66fb79d24fc940a86d060c0185
Puja Ramanathan <puja.ramanathan@gmail.com>
2020-02-27 13:08:25 -0800

    move map code to separate class

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const MyClass = require('./my-class');
 const myClassInstance = new MyClass();
 myClassInstance.sayHi();
 
-const neighborhoods = require('../data/neighbourhoods.geo.json');
+// Draw map
+const Map = require('./map');
+const myMap = new Map();
+myMap.drawMap();
 
-const mapWidth = 640;
-const mapHeight = 600;
-
+// listings csv includes id, name, host_id, host_name, neighbourhood_group, neigbhorhood,
+// latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review,
+// reviews_per_month, calculated_host_listings_count, property_type, guests_included, amenities
 d3.csv("listings_small.csv")
   .then((data) => {
     console.log('Dynamically loaded CSV data', data);
@@ -20,46 +23,4 @@ $('#fullpage').fullpage({
   anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
   navigation: true,
   navigationPosition: 'right'
-});
-
-var projection = d3.geoMercator()
-  .center([-73.94, 40.70])
-  .scale(60000)
-  .translate([mapWidth / 2, mapHeight / 2]);
-var geoPath = d3.geoPath().projection(projection);
-
-d3.select("#map-svg").selectAll("path")
-  .data(neighborhoods.features)
-  .enter()
-  .append('path')
-  .attr("d", geoPath)
-  .attr("id", function(d) {
-    return d.properties.neighbourhood; // neighborhood name
-  })
-  .attr("class", function(d) {
-    return d.properties.neighbourhood_group; // borough name
-  })
-  .style("stroke", "#ffffff") // set outline to be white
-  .on("mouseover", handleMouseOver)
-  .on("mouseout", handleMouseOut);
-
-  // Display the neighborhood and borough name on mouseover
-  function handleMouseOver(d) {
-    d3.select(this).style("fill", "#d2b48c");
-    d3.select("#selection").text("Neighborhood: " + this.id + ", Borough: " + d.properties.neighbourhood_group);
-  }
-
-  // Reset the visual to black fill on mouseout
-  function handleMouseOut(d) {
-    d3.select(this).style("fill", "black");
-    d3.select("#selection").text("Neighborhood: none selected, Borough: none selected");
-  }
-
-$('svg path').tipsy({
-    gravity: 'w',
-    html: true,
-    title: function () {
-      var d = this.__data__;
-      return 'Neighborhood: ' + this.id + '<br>' + 'Borough: ' + d.properties.neighbourhood_group;
-  }
 });
---
+++ src/map.js
@@ -0,0 +1,54 @@
+const d3 = require('d3')
+const mapWidth = 640;
+const mapHeight = 600;
+const neighborhoods = require('../data/neighbourhoods.geo.json');
+
+class Map {
+  constructor() {}
+
+  drawMap() {
+    var projection = d3.geoMercator()
+      .center([-73.94, 40.70])
+      .scale(60000)
+      .translate([mapWidth / 2, mapHeight / 2]);
+    var geoPath = d3.geoPath().projection(projection);
+
+    d3.select("#map-svg").selectAll("path")
+      .data(neighborhoods.features)
+      .enter()
+      .append('path')
+      .attr("d", geoPath)
+      .attr("id", function(d) {
+        return d.properties.neighbourhood; // neighborhood name
+      })
+      .attr("class", function(d) {
+        return d.properties.neighbourhood_group; // borough name
+      })
+      .style("stroke", "#ffffff") // set outline to be white
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut);
+
+      // Display the neighborhood and borough name on mouseover
+      function handleMouseOver(d) {
+        d3.select(this).style("fill", "#d2b48c");
+        d3.select("#selection").text("Neighborhood: " + this.id + ", Borough: " + d.properties.neighbourhood_group);
+      }
+
+      // Reset the visual to black fill on mouseout
+      function handleMouseOut(d) {
+        d3.select(this).style("fill", "black");
+        d3.select("#selection").text("Neighborhood: none selected, Borough: none selected");
+      }
+
+    $('svg path').tipsy({
+        gravity: 'w',
+        html: true,
+        title: function () {
+          var d = this.__data__;
+          return 'Neighborhood: ' + this.id + '<br>' + 'Borough: ' + d.properties.neighbourhood_group;
+      }
+    });
+  }
+}
+
+module.exports = Map;
