ce4da22edbb1211832aa7c7fb3c0170f004f2f17
Puja Ramanathan <puja.ramanathan@gmail.com>
2020-03-12 01:00:08 -0700

    Revert "build project"
    This reverts commit ca6d05ac260c59422542dbde8ae79088f2292a93.

--- docs/piechart.d1fa9172.js
+++ docs/piechart.d1fa9172.js
@@ -29166,7 +29166,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54357" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52379" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
